50485f0aa7fa08d90836dcd75877f133f53d7815
France Angelo Joel M. Reyes <134769738+afgeloo@users.noreply.github.com>
2025-08-11 02:33:11 +0800

    changed credentials for rds

--- tara-kabataan-backend/config/db.php
+++ tara-kabataan-backend/config/db.php
@@ -1,14 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "tk_webapp";
+header("Content-Type: application/json; charset=utf-8");
+$host = "tara-kabataan-rds.cdy2k86gkg9l.ap-southeast-2.rds.amazonaws.com";
+$port = 3306; 
+$user = "tkadmin";
+$pass = "tkwebapp2025";
+$db   = "tk_webapp";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = mysqli_init();
+mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 5);
 
-if ($conn->connect_error) {
-    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+if (!@mysqli_real_connect($conn, $host, $user, $pass, $db, $port)) {
+  http_response_code(500);
+  echo json_encode(["error"=>"DB connect failed","code"=>mysqli_connect_errno(),"msg"=>mysqli_connect_error()]);
+  exit;
 }
-
-header("Content-Type: application/json");
-?>
+$conn->set_charset("utf8mb4");
