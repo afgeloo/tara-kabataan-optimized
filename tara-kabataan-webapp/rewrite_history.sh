88f00ef5e0b146d4dfe28305785dc1dae43a64af
Angelo Reyes <fajmreyes@gmail.com>
2026-04-07 01:41:48 +0800

    Save current work before history rewrite

---
+++ tara-kabataan-webapp/rewrite_history.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+
+
+git filter-branch --env-filter '
+
+
+
+OLD_EMAIL="afgeloo@gmail.com"
+
+CORRECT_NAME="Angelo Reyes"
+
+CORRECT_EMAIL="fajmreyes@gmail.com"
+
+
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+
+then
+
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+
+fi
+
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+
+then
+
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+
+fi
+
+' --tag-name-filter cat -- --branches --tags
